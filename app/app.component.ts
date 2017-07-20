224d89bf85edfac56a5d964629890fa5155df6e6
zhangjiale <zhang.jiale@britishcouncil.org.cn>
2017-07-20 11:04:29 +0800

    commit compoent.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,27 @@
 import { Component } from "@angular/core";
+import { Site } from "./Site";
+// @Component({
+//   selector: "my-app",
+//   template: "<site-form></site-form>"
+// })
+// export class AppComponent { }
 
 @Component({
-  selector: "my-app",
-  template: "<site-form></site-form>"
+    selector : "my-app",
+    template : "<h1>{{ title }}</h1>"
+    + "<h2>My favorite web site: {{ mySite }}</h2>"
+    + "<p>Site List:</p>"
+    + "<ul>"
+    + "<li *ngFor='let site of sites'>  {{ site.name }} </li>"
+    + "</ul>"
+    + "<p *ngIf='sites.length > 2'>Your sites more than 2.</p>"
 })
-export class AppComponent { }
-
-// @Component({
-//     selector : "my-app",
-//     template : "<h1>{{ title }}</h1>"
-//     + "<h2>My favorite web site: {{ mySite }}</h2>"
-//     + "<p>Site List:</p>"
-//     + "<ul>"
-//     + "<li *ngFor='let site of sites'>  {{ site.name }} </li>"
-//     + "</ul>"
-//     + "<p *ngIf='sites.length > 2'>Your sites more than 2.</p>"
-// })
-// export class AppComponent { 
-//     title = "Site list";
-//     mySite = "My first angular2 app.";
-//     sites = [
-//         new Site(1, "StackOverflow."),
-//         new Site(2, "GitHub."),
-//         new Site(3, "CnBlogs.")
-//     ];
-// }
+export class AppComponent { 
+    title = "Site list";
+    mySite = "My first angular2 app.";
+    sites = [
+        new Site(1, "StackOverflow."),
+        new Site(2, "GitHub."),
+        new Site(3, "CnBlogs.")
+    ];
+}
